508b52053ee60596310f6e72b55db8d2e177f220
Colm <colmtoreilly@gmail.com>
2026-08-27 15:07:17 +0100

    Final to-do app

--- todo.py
+++ todo.py
@@ -1,30 +1,19 @@
-tasks = []
+import json
+from datetime import date
+today = date.today()
 
-tasks.append({
-    'id':1,
-    'description': 'Learn Python',
-    'completed': False
-})
-tasks.append({
-    'id':2,
-    'description': 'Build a web app',
-    'completed': False
-})
-tasks.append({
-    'id':3,
-    'description': 'Practice SQL',
-    'completed': False
-})
-tasks.append({
-    'id':4,
-    'description': 'Go to the gym',
-    'completed': False
-})
-tasks.append({
-    'id':5,
-    'description': 'Shoot around',
-    'completed': False
-})
+def save_tasks():
+        with open('todo.json', 'w') as file:
+               json.dump(tasks,file,indent=4)
+
+def load_tasks():
+	try:
+	        with open('todo.json', 'r') as file:
+        	        return json.load(file)
+	except (FileNotFoundError, json.JSONDecodeError):
+		return[]
+
+tasks = load_tasks()
 
 def add_task():
     new_id = len(tasks) + 1
@@ -35,10 +24,12 @@ def add_task():
         'description': description,
         'completed': False
     })
+    save_tasks()
+
 def view_tasks():
     for task in tasks:
-        status = 'Completed' if task['completed'] else 'Not Completed'
-        print(f'ID: {task['id']}, Description: {task['description']}, Status: {status}')
+        status = '[✓]' if task['completed'] else '[ ]'
+        print(f'Task {task['id']},{task['description']},{status}')
 
 def complete_task():
 	status_change  = int(input('Which task id have you completed?:'))
@@ -47,7 +38,8 @@ def complete_task():
 			task['completed'] = True
 			print('Task Completed, well done!')
 			break
-		
+	save_tasks()
+
 def remove_task(tasks):
 	which = int(input('Which task would you like to remove?:'))
 	for task in tasks:
@@ -59,28 +51,31 @@ def remove_task(tasks):
 		if task['id'] > which:
 			task['id'] -= 1
 		elif task['id'] == len(tasks):
-			break			
+			break
+	save_tasks()
+
 while True:
-    
-    print('\n=== To-Do List ===')
-    print('1. View tasks')
-    print('2. Add task')
-    print('3. Change Completion Status')
-    print('4. Delete task')
-    print('5. Exit')
 
-    choice = input('Choose an option:')
+	print('\n=== To-Do List ===')
+	print(today)
+	print('1. View tasks')
+	print('2. Add task')
+	print('3. Change Completion Status')
+	print('4. Delete task')
+	print('5. Exit')
 
-    if choice == '1':
-        view_tasks()
-    elif choice == '2':
-        add_task()
-    elif choice == '3':
-        complete_task()
-    elif choice == '4':
-        remove_task(tasks)
-    elif choice == '5':
-        print('Goodbye!')
-        break
-    else:
-        print('Invalid choice. Please try again.')
+	choice = input('Choose an option:')
+
+	if choice == '1':
+		view_tasks()
+	elif choice == '2':
+		add_task()
+	elif choice == '3':
+		complete_task()
+	elif choice == '4':
+		remove_task(tasks)
+	elif choice == '5':
+		print('Goodbye!')
+		break
+	else:
+		print('Invalid choice. Please try again.')
